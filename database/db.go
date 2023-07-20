05656dd7e444159f9957829b8c3969cdd2970c9d
tanmay000009 <tanmayvyas009@gmail.com>
2023-07-21 00:13:15 +0530

    use Roles in AutoMigrate

--- database/db.go
+++ database/db.go
@@ -34,7 +34,7 @@ func Connect() {
 	}
 
 	log.Println("Running database migrations")
-	err = db.AutoMigrate(&model.User{}, &model.Org{})
+	err = db.AutoMigrate(&model.User{}, &model.Org{}, &model.Role{})
 	if err != nil {
 		log.Fatal("Migration failed.\n", err)
 		os.Exit(1)
