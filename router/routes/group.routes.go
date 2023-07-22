2b2484aca4ffd92e1b51642083ba3ff97f142b72
tanmay000009 <tanmayvyas009@gmail.com>
2023-07-22 12:47:39 +0530

    rename groupRouter

--- router/routes/group.routes.go
+++ router/routes/group.routes.go
@@ -8,14 +8,15 @@ import (
 )
 
 func SetupGroupRoutes(router fiber.Router) {
-	rolesRouter := router.Group("/group", middleware.CheckJWT)
+	groupRouter := router.Group("/group", middleware.CheckJWT)
 
-	rolesRouter.Get("/", groupHandler.GetAllGroups)
-	rolesRouter.Get("/:id", groupHandler.GetGroupById)
-	rolesRouter.Post("/", groupHandler.CreateGroup)
-	rolesRouter.Post("/excel", groupHandler.SeedGroupsFromExcel)
-	rolesRouter.Post("/csv", groupHandler.SeedGroupsFromCSV)
-	rolesRouter.Delete("/:id", groupHandler.DeleteGroupById)
-	rolesRouter.Post("/role/add", groupHandler.AddRoleToGroup)
-	rolesRouter.Delete("/role/remove", groupHandler.DeleteRoleFromGroup)
+	groupRouter.Get("/", groupHandler.GetAllGroups)
+	groupRouter.Get("/:id", groupHandler.GetGroupById)
+	groupRouter.Post("/", groupHandler.CreateGroup)
+	groupRouter.Post("/test", groupHandler.TestUserGroup)
+	groupRouter.Post("/excel", groupHandler.SeedGroupsFromExcel)
+	groupRouter.Post("/csv", groupHandler.SeedGroupsFromCSV)
+	groupRouter.Delete("/:id", groupHandler.DeleteGroupById)
+	groupRouter.Post("/role/add", groupHandler.AddRoleToGroup)
+	groupRouter.Delete("/role/remove", groupHandler.DeleteRoleFromGroup)
 }
